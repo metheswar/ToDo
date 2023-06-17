1034c3d1b7176153d4e760305b6eee7aea28f9be
methi123 <Metheswar@gmail.com>
2023-06-17 19:44:45 +0530

    to-do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Content from "./Content";
+import Header from "./Header";
+import Input from "./Input";
+import Footer from "./Footer";
+import "./styles.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    const storedTodo = localStorage.getItem("todo");
+    if (storedTodo) {
+      setTodo(JSON.parse(storedTodo));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todo.length > 0) {
+      localStorage.setItem("todo", JSON.stringify(todo));
+    }
+  }, [todo]);
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== "") {
+      const newItem = {
+        id: todo.length > 0 ? todo[todo.length - 1].id + 1 : 1, // Generate a unique ID based on the last item's ID or start with 1 if the list is empty
+        item: inputValue.trim(),
+      };
+      setTodo([...todo, newItem]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteItem = (id) => {
+    const updatedTodo = todo.filter((item) => item.id !== id);
+    setTodo(updatedTodo);
+    localStorage.setItem("todo", JSON.stringify(updatedTodo));
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Input
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        handleAddItem={handleAddItem}
+      />
+      <Content todo={todo} handleDeleteItem={handleDeleteItem} />
+      <Footer
+      length={todo.length}
+      />
     </div>
   );
 }
